5e3555c3c85271dfe4f6be220b2c839f86594a97
timos lanitis <timl@sitecore.net>
2023-05-19 13:21:47 +0300

    a

---
+++ src/Item.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+class Item extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            content: this.props.items.map(item=>item.description),
+            title: this.props.items.map(item=>item.title),
+            buttonLabel: this.props.items.map(item=>item.buttonLabel),
+            image: this.props.items.map(item=>item.image1)
+        }
+    }
+    render() {
+        const className = 'item level' + this.props.level;
+        const item = this.props.id;
+        const id = "slide" + this.props.id.toString();
+
+        if (this.props.variant !== 'secondary') {
+            return (
+                <div className={className} id={id} style={{
+                    color:'white',
+                    display:'flex', flexDirection:'column', justifyContent: 'center',
+                    backgroundSize: '100%',
+                    backgroundPosition: 'center',
+                    backgroundImage: `url("${this.state.image[item]?.results?.[0]?.fileUrl}")`
+                }}>
+                    <h2 style={{marginTop:'2px'}}>{this.state.title[item]}</h2>
+                    <p style={{marginTop:0}}>
+                        {this.state.content[item]}
+                        {this.props.noButtons !== 'true' && (<div>
+                            <button style={{
+                                marginTop: '18px',
+                                display: 'inline',
+                                top: '0',
+                                position: 'relative',
+                            }}>{this.state.buttonLabel[item]}
+                            </button>
+                        </div>)}
+                    </p>
+                </div>
+            );
+        } else {
+            return (
+                <div className={className} id={id}  style={{display: 'flex', flexDirection: 'row'}}>
+                    <div style={{width:'50%', display:'flex', flexDirection:'column', alignSelf: 'center'}}>
+                        <h2 style={{marginTop:'2px'}}>{this.state.title[item]}</h2>
+                        <p style={{marginTop:0, padding:'10px'}}>
+                            {this.state.content[item]}
+                            {this.props.noButtons !== 'true'  && (<div>
+                                <button style={{
+                                    marginTop: '18px',
+                                    display: 'inline',
+                                    top: '0',
+                                    position: 'relative',
+                                }}>{this.state.buttonLabel[item]}
+                                </button>
+                            </div>)}
+                        </p>
+                    </div>
+                    <div style={{width:'50%',
+                        backgroundSize: '1000px',
+                        backgroundPosition: 'center',
+                        backgroundImage: `url("${this.state.image[item]?.results?.[0]?.fileUrl}")` }}>
+                    </div>
+                </div>
+            );
+        }
+    }
+}
+
+export default Item
--- src/Slider.js
+++ src/Slider.js
@@ -1,74 +1,7 @@
 import React from 'react'
 import * as FEAAS from "@sitecore-feaas/clientside/react"
 import {fetchData} from "./fetchData";
-
-class Item extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: this.props.items.map(item=>item.description),
-      title: this.props.items.map(item=>item.title),
-      buttonLabel: this.props.items.map(item=>item.buttonLabel),
-      image: this.props.items.map(item=>item.image1)
-    }
-  }
-  render() {
-    const className = 'item level' + this.props.level;
-    const item = this.props.id;
-    const id = "slide" + this.props.id.toString();
-
-    if (this.props.variant !== 'secondary') {
-      return (
-          <div className={className} id={id} style={{
-            color:'white',
-            display:'flex', flexDirection:'column', justifyContent: 'center',
-            backgroundSize: '100%',
-            backgroundPosition: 'center',
-            backgroundImage: `url("${this.state.image[item]?.results?.[0]?.fileUrl}")`
-          }}>
-            <h2 style={{marginTop:'2px'}}>{this.state.title[item]}</h2>
-            <p style={{marginTop:0}}>
-              {this.state.content[item]}
-              {this.props.noButtons !== 'true' && (<div>
-                <button style={{
-                  marginTop: '18px',
-                  display: 'inline',
-                  top: '0',
-                  position: 'relative',
-                }}>{this.state.buttonLabel[item]}
-                </button>
-              </div>)}
-            </p>
-          </div>
-      );
-    } else {
-      return (
-          <div className={className} id={id}  style={{display: 'flex', flexDirection: 'row'}}>
-            <div style={{width:'50%', display:'flex', flexDirection:'column', alignSelf: 'center'}}>
-              <h2 style={{marginTop:'2px'}}>{this.state.title[item]}</h2>
-              <p style={{marginTop:0, padding:'10px'}}>
-                {this.state.content[item]}
-                {this.props.noButtons !== 'true'  && (<div>
-                  <button style={{
-                    marginTop: '18px',
-                    display: 'inline',
-                    top: '0',
-                    position: 'relative',
-                  }}>{this.state.buttonLabel[item]}
-                  </button>
-                </div>)}
-              </p>
-            </div>
-          <div style={{width:'50%',
-            backgroundSize: '1000px',
-            backgroundPosition: 'center',
-            backgroundImage: `url("${this.state.image[item]?.results?.[0]?.fileUrl}")` }}>
-          </div>
-          </div>
-      );
-    }
-  }
-}
+import Item from "./Item";
 
 class Slider extends React.Component {
   constructor(props){
@@ -125,7 +58,6 @@ class Slider extends React.Component {
   }
 
   render(){
-    console.log(this.state)
     const index = this.state.active;
     const level = index - 1;
     if (!this.state.items.length) return (<div>loading</div>)
